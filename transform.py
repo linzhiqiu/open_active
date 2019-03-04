ba4e719a49e9eb78f615a8cf65ce5c56a92e4e89
linzhiqiu <zl279@cornell.edu>
2019-03-04 14:00:50 -0500

    [add] transform for CIFAR100

---
+++ transform.py
@@ -0,0 +1,19 @@
+import torchvision
+import torchvision.transforms as transforms
+
+def get_transform_dict(data):
+    assert data in ["CIFAR10", "CIFAR100"]
+    if data in ["CIFAR10", "CIFAR100"]:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    else:
+        raise ValueError("Dataset not supported")
