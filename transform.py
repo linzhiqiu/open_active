111ee0115cc660115030489c5df525db0bcfe794
linzhiqiu <zl279@cornell.edu>
2020-05-19 18:35:52 -0400

    [change] transform for cifar100

--- transform.py
+++ transform.py
@@ -16,14 +16,14 @@ def get_transform_dict(data):
 def get_cifar_transform():
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
-        transforms.Resize(32),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     return transform_train, transform_test
 
