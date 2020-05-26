54788a730af39c61f88cd42a7c6a4bc432a89f5d
linzhiqiu <zl279@cornell.edu>
2020-05-26 02:16:15 -0400

    [in implement] open set eval

--- analysis.sh
+++ analysis.sh
@@ -6,3 +6,7 @@ python analysis.py CIFAR100 --analysis_trainer cosine_network --budget_mode 1_5_
 # No finetune mode
 python analysis.py CIFAR100 --analysis_trainer softmax_network --train_mode no_finetune --budget_mode 1_5_10_20_50_100
 python analysis.py CIFAR100 --analysis_trainer cosine_network --train_mode no_finetune --budget_mode 1_5_10_20_50_100
+
+# default 200 epoch lr 0.1 finetune
+python analysis.py CIFAR100 --analysis_trainer softmax_network --train_mode default_lr01_200eps --budget_mode 1_5_10_20_50_100
+python analysis.py CIFAR100 --analysis_trainer cosine_network --train_mode default_lr01_200eps --budget_mode 1_5_10_20_50_100
